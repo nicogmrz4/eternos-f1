1d5c854af0623faa63583f8230d2ac4c7aad5e0f
Nicolas <nicolasgalarza40@gmail.com>
2024-05-26 17:16:08 -0300

    feat: add display result feature

--- src/components/TrackCard.vue
+++ src/components/TrackCard.vue
@@ -1,33 +1,49 @@
 <script setup lang="ts">
 import Badge from '@/components/atoms/Badge.vue';
 import type { DriverResultInterface } from '@/interfaces/driverResult';
-const props = defineProps({
-    order: Number,
-    name: String,
-    circuit: String,
-    flag: String,
-    isRaced: Boolean,
-    result: () => Array<DriverResultInterface>
-});
+import { ref } from 'vue';
+
+interface Props {
+    order: number,
+    name: string,
+    circuit: string,
+    flag: string,
+    isRaced: boolean,
+    result: DriverResultInterface[]
+}
+
+const props = defineProps<Props>();
+const showResult = ref(false);
 </script>
 
 <template>
-    <div class="track-card">
-        <span class="track-card__order">#{{ order  }}</span>
+    <div :class="['track-card', isRaced ? 'pointer' : ' ']" @click="showResult = !showResult">
+        <span class="track-card__order">#{{ order }}</span>
         <span :class="[flag, 'track-card__flag']"></span>
         <div class="track-card__info">
             <div class="name">{{ name }}</div>
             <div class="circuit card-text-muted">{{ circuit }}</div>
         </div>
         <Badge class="track-card__status" :color="isRaced ? 'success' : 'pending'">
-            {{ isRaced ? 'Completada' : 'Pendiente'  }}
+            {{ isRaced ? 'Completada' : 'Pendiente' }}
         </Badge>
+        <Transition>
+            <div v-if="showResult && isRaced" class="track-card__result">
+                <div v-for="r in result" :key="r.driver.id"
+                    :class="['driver-result', r.dnf ? 'dnf' : '', r.fastLap ? 'fast-lap' : '']">
+                    <span class="pos">{{ r.dnf ? 'DNF' : r.position }}</span>
+                    <span class="name">{{ r.driver.name }}</span>
+                    <span class="team">{{ r.driver.team }}</span>
+                </div>
+            </div>
+        </Transition>
     </div>
 </template>
 
 <style scoped>
 .track-card {
     display: flex;
+    flex-wrap: wrap;
     background-color: var(--card-color);
     align-items: center;
     gap: var(--card-padding);
@@ -35,6 +51,11 @@ const props = defineProps({
     border-radius: var(--card-border-radius);
     padding: var(--card-padding);
     transition: opacity 0.2s ease-out, transform 0.2s ease-out;
+    overflow: hidden;
+}
+
+.pointer {
+    cursor: pointer;
 }
 
 .track-card--raced {
@@ -72,5 +93,61 @@ const props = defineProps({
 
 .track-card__status {
     margin-left: auto;
+}
+
+.track-card__result {
+    width: 100%;
+}
+
+.driver-result {
+    display: flex;
+    padding: 1px 12px;
+}
+
+.driver-result > .pos {
+    width: 40px;
+}
+
+.driver-result > .name {
+    width: 120px;
+}
+
+.dnf {
+    opacity: .6;
+}
+
+.fast-lap {
+    font-weight: 700;
+    background-color: #c92bdf;
+    border-radius: 8px;
+}
+
+
+.v-enter-from {
+    max-height: 0;
+    opacity: 0;
+}
+
+.v-enter-active {
+    transition: max-height 400ms linear, opacity 0.2s ease-out 150ms;
+}
+
+.v-enter-to {
+    max-height: 500px;
+    opacity: 1;
+}
+
+.v-leave-from {
+    max-height: 500px;
+    opacity: 1;
+}
+
+.v-leave-active {
+    transition: max-height 400ms linear, opacity 0.2s ease-in;
+}
+
+.v-leave-to {
+    max-height: 0;
+    opacity: 0;
 }
 </style>
