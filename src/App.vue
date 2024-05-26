1526b4742c6fefe9738c8f76ba83594274cc37d3
Nicolas <nicolasgalarza40@gmail.com>
2024-05-26 19:26:54 -0300

    feat: mobile navbar implemented

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
-  <nav>
-    <ul>
-      <li><RouterLink to="/">Pilotos</RouterLink></li>
-      <li><RouterLink to="/teams-championship">Equipos</RouterLink></li>
-      <li><RouterLink to="/calendar">Calendario</RouterLink></li>
-    </ul>
-  </nav>
+  <Navbar/>
   <main>
     <RouterView v-slot="{ Component }"> 
       <Transition name="router">
@@ -36,43 +31,6 @@ main {
 main > * {
   width: 100%;
   padding-bottom: 3em;
-}
-
-nav {
-  width: 100%;
-  height: 80px;
-  background-color: #082479;
-  padding: 1rem;
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
-}
-
-ul {
-  list-style: none;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-}
-
-ul > li > a {
-  text-decoration: none;
-  color: white;
-  padding: 1em;
-  opacity: .5;
-  transition: opacity 200ms ease-in;
-}
-
-ul > li > a:hover {
-  opacity: .8;
-
-}
-
-.router-link-active {
-  opacity: 1;
 }
 
 .router-enter-from {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav>
+    <ul>
+      <li><RouterLink to="/">Pilotos</RouterLink></li>
+      <li><RouterLink to="/teams-championship">Equipos</RouterLink></li>
+      <li><RouterLink to="/calendar">Calendario</RouterLink></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+  height: 60px;
+  background-color: #082479;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+}
+
+ul {
+  list-style: none;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+ul > li > a {
+  text-decoration: none;
+  color: white;
+  padding: 1em;
+  opacity: .5;
+  transition: opacity 200ms ease-in;
+}
+
+ul > li > a:hover {
+  opacity: .8;
+  
+}
+
+.router-link-active {
+  opacity: 1;
+}
+
+@media screen and (max-width: 768px) {
+  nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px -14px 28px, rgba(0, 0, 0, 0.22) 0px -10px 10px;
+  }
+  ul > li > a {
+    text-decoration: none;
+    color: white;
+    padding: 1em;
+    opacity: .5;
+    transition: opacity 200ms ease-in;
+  }
+}
+</style>
