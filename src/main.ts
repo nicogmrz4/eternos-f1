03e1aca8d9c34bf16774d15b9bb6bd2793e526c9
Nicolas <nicolasgalarza40@gmail.com>
2024-05-26 14:05:08 -0300

    feat:

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { IoPerson, IoShield, IoCalendarSharp } from "oh-vue-icons/icons";
+
+addIcons(IoPerson, IoShield, IoCalendarSharp);
 
 const app = createApp(App)
-
 app.use(router)
-
+app.component("v-icon", OhVueIcon);
 app.mount('#app')
