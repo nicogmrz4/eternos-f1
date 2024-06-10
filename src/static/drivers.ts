3b9f3a74ea9332610c80161b7403cc938ea0ac42
Nicolas <nicolasgalarza40@gmail.com>
2024-06-09 23:30:54 -0300

    feat: :sparkles: load initial data

--- src/static/drivers.ts
+++ src/static/drivers.ts
@@ -13,25 +13,46 @@ import {
 } from "./teams";
 import { Driver } from "@/dto/driverDTO";
 
-export const JOHN = new Driver(1, 'John', MERCEDES);
-export const EMMA = new Driver(2, 'Emma', FERRARI);
-export const MICHAEL = new Driver(3, 'Michael', RED_BULL);
-export const SOFIA = new Driver(4, 'Sophia', MCLAREN);
-export const WILLIAM = new Driver(5, 'William', RENAULT);
-export const OLIVIA = new Driver(6, 'Olivia', ALPHATAURI);
-export const JAMES = new Driver(7, 'James', RACING_POINT); 
-export const AVA = new Driver(8, 'Ava', ALFA_ROMEO);
-export const ALEXANDER = new Driver(9, 'Alexander', HAAS);
-export const MIA = new Driver(10, 'Mia', WILLIAMS);
-export const MARCELO = new Driver(11, 'Marcelo', MERCEDES);
-export const JOE = new Driver(12, 'Joe', FERRARI);
-export const DANIEL = new Driver(13, 'Daniel', RENAULT);
-export const LUIS = new Driver(14, 'Luis', ALPHATAURI);
-export const LUCAS = new Driver(15, 'Lucas', RACING_POINT);
-export const GABRIEL = new Driver(16, 'Gabriel', ALFA_ROMEO);
-export const KATE = new Driver(17, 'Kate', HAAS);
-export const LANDO = new Driver(18, 'Lando', WILLIAMS);
-export const LILIAN = new Driver(19, 'Lilian', RED_BULL);
-export const IVAN = new Driver(20, 'Iván', MCLAREN);
+export const COLO = new Driver(1, 'Colo', RED_BULL);
+export const EMI = new Driver(2, 'Emi', RED_BULL);
+export const DAMITOX = new Driver(3, 'Damitox', MCLAREN);
+export const GALLA = new Driver(4, 'Galla', MCLAREN);
+export const EMMA = new Driver(5, 'Emma', WILLIAMS);
+export const MAX = new Driver(6, 'Max', WILLIAMS);
+export const NICOGMERZ4 = new Driver(7, 'nicogmerz4', RACING_POINT); 
+export const PABLITO = new Driver(8, 'Pablito', RACING_POINT);
+export const SHINO = new Driver(9, 'Shino', ALFA_ROMEO);
+export const FER = new Driver(10, 'Fer', ALFA_ROMEO);
+export const FACULANUS = new Driver(11, 'Faculanus', FERRARI);
+export const JUANMA = new Driver(12, 'Juanma', FERRARI);
+export const JULIAN_SOSA = new Driver(13, 'Julián Sosa', HAAS);
+export const SEBA_HUNTER = new Driver(14, 'Seba Hunter', HAAS);
+export const KURI = new Driver(15, 'Kuri', ALPHATAURI);
+export const RYU = new Driver(16, 'Ryu', ALPHATAURI);
+export const LUCY = new Driver(17, 'Lucy', MERCEDES);
+export const BLADEBAHIA = new Driver(18, 'Bladebahia', MERCEDES);
+export const PEPPA = new Driver(19, 'Peppa', RENAULT);
+export const PIOR = new Driver(20, 'Iván', RENAULT);
 
-export const drivers = [JOHN, EMMA, MICHAEL, SOFIA, WILLIAM, OLIVIA, JAMES, AVA, ALEXANDER, MIA, MARCELO, JOE, DANIEL, LUIS, LUCAS, GABRIEL, KATE, LANDO, LILIAN, IVAN];
+export const drivers = [
+  COLO,
+  EMI,
+  DAMITOX,
+  GALLA,
+  EMMA,
+  MAX,
+  NICOGMERZ4,
+  PABLITO,
+  SHINO,
+  FER,
+  FACULANUS,
+  JUANMA,
+  JULIAN_SOSA,
+  SEBA_HUNTER,
+  KURI,
+  RYU,
+  LUCY,
+  BLADEBAHIA,
+  PEPPA,
+  PIOR
+];
--- src/static/tracks.ts
+++ src/static/tracks.ts
@@ -1,29 +1,104 @@
-import { hungaryGPResult } from "./results/hungary"
-import { belgiumGPResult } from "./results/belgium"
 import type { TrackInterface } from "@/interfaces/track"
 import { TrackDTO } from "@/dto/trackDTO"
 
 
 export default <TrackInterface[]> [
     new TrackDTO(
-        'GP Hungría',
-        'Hungaroring',
-        'fi fi-hu',
-        true,
-        hungaryGPResult
-    ),
-    new TrackDTO(
-        'GP Bélgica',
-        'Circuito de Spa-Francorchamps',
-        'fi fi-be',
-        true,
-        belgiumGPResult
-    ),
-    new TrackDTO(
         'GP Holanda',
         'Circuito de Zandvoort',
         'fi fi-nl',
         false,
         []
-    )
+    ),
+    new TrackDTO(
+        'GP Gran Bretaña',
+        'Circuito de Silverstone',
+        'fi fi-gb',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP España',
+        'Circuito de Barcelona-Cataluña',
+        'fi fi-es',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP Japón',
+        'Circuito de Suzuka',
+        'fi fi-jp',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP Mexico',
+        'Autodromo Hermanos Rodríguez',
+        'fi fi-mx',
+        false,
+        []
+    ),    
+    new TrackDTO(
+        'GP Canadá',
+        'Circuito Gilles Villeneuve',
+        'fi fi-ca',
+        false,
+        []
+    ),    
+    new TrackDTO(
+        'GP Abu Dhabi',
+        'Circuito Yas Marina',
+        'fi fi-ae',
+        false,
+        []
+    ),    
+    new TrackDTO(
+        'GP Estados Unidos',
+        'Circuito de las Américas',
+        'fi fi-us',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP Azerbaiyán',
+        'Circuito callejero de Bakú',
+        'fi fi-az',
+        false,
+        []
+    ),    
+    new TrackDTO(
+        'GP Italia',
+        'Autodromo internacional de Monza',
+        'fi fi-it',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP Bélgica',
+        'Circuito de Spa-Francorchamps',
+        'fi fi-be',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP Singapur',
+        'Circuito callejero de Marina Bay',
+        'fi fi-sg',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP Vietnam',
+        'Circuito callejero de Hanói',
+        'fi fi-vn',
+        false,
+        []
+    ),
+    new TrackDTO(
+        'GP Australia',
+        'Circuito de Albert Park',
+        'fi fi-au',
+        false,
+        []
+    ),
 ]
