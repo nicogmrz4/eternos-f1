13e47b5056d35736a34fe1ab1952cbe3f61529dc
Nicolas <nicolasgalarza40@gmail.com>
2025-01-29 13:13:26 -0300

    feat: update penalty points for belgium GP and italian GP

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -11,7 +11,7 @@ export const useGlobalStore = defineStore("globalStore", {
     teams: [] as TeamInterface[],
     drivers: [] as DriverInterface[],
     currentSeason: "season-9-a",
-    lastUpdate: "230120251533", // ddmmyyyyhhmm
+    lastUpdate: "290120251311", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
