2e1e2c311bd7f957cbed8aa4ce1c3a1f9aa1db06
Nicolas <nicolasgalarza40@gmail.com>
2025-01-30 13:51:44 -0300

    feat: update barein gp league b

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -11,7 +11,7 @@ export const useGlobalStore = defineStore("globalStore", {
     teams: [] as TeamInterface[],
     drivers: [] as DriverInterface[],
     currentSeason: "season-9-a",
-    lastUpdate: "290120251311", // ddmmyyyyhhmm
+    lastUpdate: "300120251349", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
