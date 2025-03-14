1767eae9587f524f8ba6afff2ba64a74c384fa43
Nicolas Galarza <nicolasgalarza40@gmail.com>
2025-03-14 10:35:17 -0300

    feat: update league B

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -11,7 +11,7 @@ export const useGlobalStore = defineStore("globalStore", {
     teams: [] as TeamInterface[],
     drivers: [] as DriverInterface[],
     currentSeason: "season-9-a",
-    lastUpdate: "270220252238", // ddmmyyyyhhmm
+    lastUpdate: "140320251030", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
