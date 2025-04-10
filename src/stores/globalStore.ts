b45ba55f3509b7c2906d17846003e23bfd4227ad
Nicolas Galarza <nicolasgalarza40@gmail.com>
2025-04-10 15:09:53 -0300

    fix: add missing driver on bahrein sprint

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -12,7 +12,7 @@ export const useGlobalStore = defineStore("globalStore", {
     drivers: [] as DriverInterface[],
     currentSeason: "season-10",
     currentSeasonOptions: {},
-    lastUpdate: "100420251330", // ddmmyyyyhhmm
+    lastUpdate: "100420251510", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
