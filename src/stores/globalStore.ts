cae65854b51fce9f96ec059cae7fb1fddcf773be
Nicolas Galarza <nicolasgalarza40@gmail.com>
2025-04-17 00:58:41 -0300

    feat: update bahrein gp season 10

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -12,7 +12,7 @@ export const useGlobalStore = defineStore("globalStore", {
     drivers: [] as DriverInterface[],
     currentSeason: "season-10",
     currentSeasonOptions: {},
-    lastUpdate: "160420251730", // ddmmyyyyhhmm
+    lastUpdate: "170420250100", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
