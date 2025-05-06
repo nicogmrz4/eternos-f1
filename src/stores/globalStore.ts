7caac4ade13797d53967e6063c0e776ae628ec30
Nicolas Galarza <nicolasgalarza40@gmail.com>
2025-05-06 14:11:39 -0300

    feat: update australia gp

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -12,7 +12,7 @@ export const useGlobalStore = defineStore("globalStore", {
     drivers: [] as DriverInterface[],
     currentSeason: "season-10",
     currentSeasonOptions: {},
-    lastUpdate: "240420250055", // ddmmyyyyhhmm
+    lastUpdate: "060520251411", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
