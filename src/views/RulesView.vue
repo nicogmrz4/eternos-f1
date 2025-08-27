b182065db6e5f34aaed900a59a0f64d2ad13d946
Nicolas Galarza <nicolasgalarza40@gmail.com>
2025-08-27 20:34:07 -0300

    feat: add transition

--- src/views/RulesView.vue
+++ src/views/RulesView.vue
@@ -1,15 +1,25 @@
 <template>
-  <h2>Índice</h2>
-    <RouterLink to="/rules/points" style="margin-top: 25px; display: block;">Puntuación</RouterLink>
-    <RouterLink to="/rules/sprint" style="margin-top: 25px; display: block;">Carrera Sprint</RouterLink>
-    <RouterLink to="/rules/other-points" style="margin-top: 25px; display: block;">Otros Puntuaciones</RouterLink>
-    <RouterLink to="/rules/room-config" style="margin-top: 25px; display: block;">Configuración de Sala</RouterLink>
-    <RouterLink to="/rules/penalties" style="margin-top: 25px; display: block;">Penalizaciones</RouterLink>
+  <div>
+    <Transition name="change" mode="out-in">
+      <div>
+        <h2>Índice</h2>
+        <Transition name="change" mode="out-in">
+          <div>
+            <RouterLink to="/rules/points" style="margin-top: 25px; display: block;">Puntuación</RouterLink>
+            <RouterLink to="/rules/sprint" style="margin-top: 25px; display: block;">Carrera Sprint</RouterLink>
+            <RouterLink to="/rules/other-points" style="margin-top: 25px; display: block;">Otros Puntuaciones</RouterLink>
+            <RouterLink to="/rules/room-config" style="margin-top: 25px; display: block;">Configuración de Sala</RouterLink>
+            <RouterLink to="/rules/penalties" style="margin-top: 25px; display: block;">Penalizaciones</RouterLink>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { otherPoints as otherPointsStatic, pointsPerPositionV2, pointsPerPositionSprint, roomConfig as roomConfigStatic, type OtherPoints, type PointsPerPosition, type RoomConfig } from '@/static/rules';
-import { ref, type Ref } from 'vue';
+import { ref, Transition, type Ref } from 'vue';
 
 const roomConfig: Ref<RoomConfig[]> = ref(roomConfigStatic);
 
