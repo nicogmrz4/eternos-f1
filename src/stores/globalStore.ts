14352502f81602e1c2f40e70f1829b0a859c4bd8
Nicolas Galarza <nicolasgalarza40@gmail.com>
2025-09-10 12:07:17 -0300

    feat: update penalties for netherlands gp

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -12,7 +12,7 @@ export const useGlobalStore = defineStore("globalStore", {
     drivers: [] as DriverInterface[],
     currentSeason: "season-11",
     currentSeasonOptions: {},
-    lastUpdate: "040920250055", // ddmmyyyyhhmm
+    lastUpdate: "100920251206", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
