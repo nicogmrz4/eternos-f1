4596c36da4942eed45ca407feddaec022f42c3c2
Nicolas Galarza <nicolasgalarza40@gmail.com>
2025-09-21 00:32:06 -0300

    feat: update spa gp results and penalties

--- src/stores/globalStore.ts
+++ src/stores/globalStore.ts
@@ -12,7 +12,7 @@ export const useGlobalStore = defineStore("globalStore", {
     drivers: [] as DriverInterface[],
     currentSeason: "season-11",
     currentSeasonOptions: {},
-    lastUpdate: "120920251651", // ddmmyyyyhhmm
+    lastUpdate: "210920250031", // ddmmyyyyhhmm
   }),
   actions: {
     toggleShowPositions() {
