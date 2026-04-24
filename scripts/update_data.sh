757686a18317418b6ba4f19ef140ae60d3a4c44d
Nicolas Galarza <nicolasgalarza40@gmail.com>
2026-04-24 20:23:33 -0300

    fix: remove redirect

--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -11,7 +11,7 @@ select file in $files; do
     if [ -n "$file" ]; then
         read -p "Ingrese el ID de la temporada: " seasonId
         if [ -n "$seasonId" ]; then
-            data=$(curl "http://f1eternosjsongen.local/api/seasons/$seasonId/export" &> /dev/null)
+            data=$(curl "http://f1eternosjsongen.local/api/seasons/$seasonId/export")
             echo $data > $PWD/public/data/$file
         fi
 	new_date=$(date +"%d%m%Y%H%M")
